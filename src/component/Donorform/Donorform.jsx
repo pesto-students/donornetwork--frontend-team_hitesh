d9eeb953440f054a5357cb57728443382a95b0b7
Anant Bajpai <anant.bajpai009@gmail.com>
2023-03-31 13:43:51 +0530

    feat: removing header

--- src/component/Donorform/Donorform.jsx
+++ src/component/Donorform/Donorform.jsx
@@ -6,28 +6,6 @@ import { Outlet, Link } from "react-router-dom";
 const Donorform = () => {
   return (
     <>
-      <header className="header">
-        <div className="logo">
-          <img src={logo} alt="logo" />
-        </div>
-        <div className="nav">
-          <span className="homenav">
-            <Link to="/">Home</Link>
-          </span>
-          <span className="aboutnav">
-            <a href="#aboutus">About</a>
-          </span>
-          <span className="blog">
-            <Link to="/blog">Blog</Link>
-          </span>
-          <span className="searchblood">
-            <Link to="/searchbloodbank">Search blood bank</Link>
-          </span>
-          <button className="loginbtn">
-            <Link to="/loginbtn">Login</Link>
-          </button>
-        </div>
-      </header>
       <div className="form"></div>
       <div class="main-block">
         <form action="/">
